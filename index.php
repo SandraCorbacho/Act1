0c71d5f92c9aa4ceede30cafe91a6a60027a5bdd
linux <linux@debian10.4>
2020-09-28 17:14:23 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    if (isset($_POST['username']) && !empty($_POST['username'])){
+        echo $_POST['username'];
+    }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fromulario php</title>
+</head>
+<body>
+    <form action="<?= $_SERVER['PHP_SELF'];?>" method="POST">
+        <label for="username">USERNAME</label>
+        <input type="text" name="username">
+        <input type="submit" value="Envia">
+    </form>
+</body>
+</html>
